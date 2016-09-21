ecfc3c4952167ee89056a492a35f5d258da3ce1c
Darren Hall <itsdarrenhall@gmail.com>
2016-09-21 16:40:21 +0100

    chore: switch port depending on environment

--- source/server.js
+++ source/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
+var PORT = (process.env.NODE_ENV == 'production') ? 4000 : 3000;
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.listen(3000, 'localhost');
+app.listen(PORT, 'localhost', function() {
+    console.log('Listening on port ' + PORT);
+});
