d420f7970d8e9dc50d7ca6ccf31ec59697d9dcb5
Darren Hall <itsdarrenhall@gmail.com>
2016-09-23 09:12:10 +0100

    feat: add handlebars templating with initial view and layout

--- source/server.js
+++ source/server.js
@@ -1,13 +1,25 @@
+'use strict';
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
 
-var PORT = (process.env.NODE_ENV == 'production') ? 4000 : 3000;
+var hbs = exphbs.create({
+	extname: 'hbs',
+	defaultLayout: 'default'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello, it\'s me');
+    res.render('home');
 });
 
+// Production server runs on port 4000, development is port 3000
+var PORT = (process.env.NODE_ENV == 'production') ? 4000 : 3000;
+
 app.listen(PORT, 'localhost', function() {
-    console.log('Listening on port ' + PORT);
+    console.log('Listening on port yeah' + PORT);
 });
