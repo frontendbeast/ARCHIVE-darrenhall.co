e850079688cd1bb83b5de50b1d07d57f04c93069
Darren Hall <hello@darrenhall.info>
2016-09-28 21:07:09 +0100

    chore: fix bug in view directory path

--- source/server.js
+++ source/server.js
@@ -1,16 +1,21 @@
 'use strict';
+
+var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
 var app = express();
 
 var hbs = exphbs.create({
-	extname: 'hbs',
-	defaultLayout: 'default'
+        extname: 'hbs',
+        defaultLayout: 'default',
+        layoutsDir: path.join(__dirname, 'views', 'layouts'),
 });
 
 app.engine('hbs', hbs.engine);
+
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
